e1a4652a3b047b9d5076edcd8e697d7967a920d6
Biplaba <samantaraybiplaba@gmail.com>
2020-08-31 12:45:50 +0530

    schema updated

---
+++ app.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const graphqlHttp = require('express-graphql').graphqlHTTP;
+const { buildSchema } = require('graphql');
+const mongoose = require('mongoose');
+
+
+
+
+const app = express();
+app.use(bodyParser.json());
+
+
+const graphQlSchema = require('./apis/schema/graphqlSchema');
+const graphQlResolvers = require('./apis/resolver/graphqlResolver');
+
+app.use(
+    '/graphql',
+    graphqlHttp({
+      schema: graphQlSchema,
+      rootValue: graphQlResolvers,
+      graphiql: true
+    })
+  )
+  
+
+
+  mongoose.connect("mongodb+srv://Biplaba:bips1996@copycart.vkz7v.mongodb.net/TESTDB?retryWrites=true&w=majority",{useNewUrlParser: true,useUnifiedTopology: true,})
+    .then(() => {
+        app.listen(3000);
+      })
+      .catch(err => {
+        console.log(err);
+      });
+
+
